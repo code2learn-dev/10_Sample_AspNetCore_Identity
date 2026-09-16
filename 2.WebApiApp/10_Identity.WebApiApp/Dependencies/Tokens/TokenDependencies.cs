769e7dd9dc769420d537b00bf926738804a61635
code2learn-dev <broumand.payam@outlook.com>
2026-09-16 21:35:52 +0330

    add token dependencies services to web api project

---
+++ 2.WebApiApp/10_Identity.WebApiApp/Dependencies/Tokens/TokenDependencies.cs
@@ -0,0 +1,16 @@
+﻿using Identity.ApplicationService.Tokens.Services;
+using Identity.Repository.Users;
+
+namespace _10_Identity.WebApiApp.Dependencies.Tokens
+{
+    public static class TokenDependencies
+    {
+        public static IServiceCollection ConfigureTokenServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IModelValidator, ModelValidator>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            return services;
+        }
+    }
+}
--- 2.WebApiApp/10_Identity.WebApiApp/Dependencies/WebApiApplicationServices.cs
+++ 2.WebApiApp/10_Identity.WebApiApp/Dependencies/WebApiApplicationServices.cs
@@ -1,86 +1,103 @@
 ﻿using _10_Identity.WebApiApp.Dependencies.Categories;
+using _10_Identity.WebApiApp.Dependencies.Tokens;
+using Identity.ApplicationService.Tokens.Entities;
+using Identity.ApplicationService.Tokens.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 namespace _10_Identity.WebApiApp.Dependencies
 {
-    public static class WebApiApplicationServices
-    {
-        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
-        {
-            // add EF core service with Sql Server provider
-            builder.Services.AddDbContext<AcademyDbContext>(options =>
-            {
-                options.UseSqlServer("Data Source=(local);Initial Catalog=sample_aspnetcore_identity_academy;TrustServerCertificate=True;Integrated Security=True;MultipleActiveResultSets=True;");
-            });
+	public static class WebApiApplicationServices
+	{
+		public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+		{
+			// add EF core service with Sql Server provider
+			builder.Services.AddDbContext<AcademyDbContext>(options =>
+			{
+				options.UseSqlServer("Data Source=(local);Initial Catalog=sample_aspnetcore_identity_academy;TrustServerCertificate=True;Integrated Security=True;MultipleActiveResultSets=True;");
+			});
 
-            // add web api service 
-            builder.Services.AddControllers();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddEndpointsApiExplorer();
+			// add web api service 
+			builder.Services.AddControllers();
+			builder.Services.AddSwaggerGen();
+			builder.Services.AddEndpointsApiExplorer();
+
+			// add automapper service
+			builder.Services.AddAutoMapper(a => a.AddProfile(new DtoMappingProfile()));
 
 
-            // add jwt bearer authenticatin configuration
-            var jwtSection = builder.Configuration.GetSection("jwt");
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["key"]));
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-             .AddJwtBearer(bearerOptions =>
-             {
-                 bearerOptions.TokenValidationParameters = new TokenValidationParameters()
-                 {
-                     ValidIssuer = jwtSection["issuer"],
-                     ValidAudience = jwtSection["audience"],
-                     IssuerSigningKey = securityKey,
-                     ValidateLifetime = true,
-                     ValidateIssuerSigningKey = true,
-                     SaveSigninToken = true
-                 };
+			builder.Services.Configure<JwtSectionConfiguration>(
+					builder.Configuration.GetSection("jwt"));
 
-                 bearerOptions.Events = new JwtBearerEvents()
-                 {
-                     OnTokenValidated = e => { return Task.CompletedTask; },
-                     OnForbidden = e => { return Task.CompletedTask; },
-                     OnChallenge = e => { return Task.CompletedTask; },
-                     OnAuthenticationFailed = e => { return Task.CompletedTask; },
-                     OnMessageReceived = e => { return Task.CompletedTask; }
-                 };
-             });
+			// add jwt bearer authenticatin configuration
+			var jwtSection = builder.Configuration.GetSection("jwt");
+			var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["key"]));
+			builder.Services.AddAuthentication(options =>
+			{
+				options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			 .AddJwtBearer(bearerOptions =>
+			 {
+				 bearerOptions.TokenValidationParameters = new TokenValidationParameters()
+				 {
+					 ValidIssuer = jwtSection["issuer"],
+					 ValidAudience = jwtSection["audience"],
+					 IssuerSigningKey = securityKey,
+					 ValidateLifetime = true,
+					 ValidateIssuerSigningKey = true,
+					 SaveSigninToken = true
+				 };
 
-            builder.Services
-                // add common services
-                .RegisterPublicServices()
-                // add categories services
-                .RegisterCategoriesService();
+				 bearerOptions.Events = new JwtBearerEvents()
+				 {
+					 OnTokenValidated = async e =>
+					 {
+						 var tokenService = e.HttpContext.RequestServices.GetRequiredService<ITokenService>(); 
+						 var tokenResult = await tokenService.ValidateToken(e);
+						 if (!tokenResult.Result) 
+                             e.Fail(tokenResult.Errors.FirstOrDefault() ?? ""); 
+                     },
+					 OnForbidden = e => { return Task.CompletedTask; },
+					 OnChallenge = e => { return Task.CompletedTask; },
+					 OnAuthenticationFailed = e => { return Task.CompletedTask; },
+					 OnMessageReceived = e => { return Task.CompletedTask; }
+				 };
+			 });
 
-            return builder;
-        }
+			builder.Services
+				// add common services
+				.RegisterPublicServices()
+				// configure token services
+				.ConfigureTokenServices()
+				// add categories services
+				.RegisterCategoriesService();
 
-        public static WebApplication ConfigureMiddleware(this WebApplicationBuilder builder)
-        {
-            var app = builder.Build();
+			return builder;
+		}
 
-            if(app.Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
+		public static WebApplication ConfigureMiddleware(this WebApplicationBuilder builder)
+		{
+			var app = builder.Build();
 
-                app.UseSwagger();
-                app.UseSwaggerUI(config =>
-                {
-                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                    config.RoutePrefix = string.Empty;
-                });
-            }
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseStatusCodePages();
 
-            app.MapControllers();
+				app.UseSwagger();
+				app.UseSwaggerUI(config =>
+				{
+					config.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+					config.RoutePrefix = string.Empty;
+				});
+			}
 
-            return app;
-        }
-    }
+			app.MapControllers();
+
+			return app;
+		}
+	}
 }
