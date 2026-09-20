24fdc4b0beefbef462b4bf539d3ef28f1a911d4f
code2learn-dev <broumand.payam@outlook.com>
2026-09-20 11:12:11 +0330

    remove Token and Expire refresh token from UserToken table

---
+++ 0.Domain/Identity.Domain/Migrations/20260918184907_RemoveTokenFromUserToken.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTokenFromUserToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpireDate",
+                table: "UserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "UserTokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpireDate",
+                table: "UserTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "UserTokens",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- 0.Domain/Identity.Domain/Users/UserToken.cs
+++ 0.Domain/Identity.Domain/Users/UserToken.cs
@@ -4,9 +4,7 @@ namespace Identity.Domain.Users
 {
     public class UserToken : BaseEntity
     {
-        public string UserId { get; set; } = string.Empty;
-        public string Token { get; set; } = string.Empty;
-        public DateTime ExpireDate { get; set; }
+        public string UserId { get; set; } = string.Empty;  
         public bool IsActive { get; set; } = true;
 		public string RefreshToken { get; set; } = string.Empty;
 		public DateTime ExpireRefreshToken { get; set; }
